5a2d4e8ac6b570bb281543c8f24cf374c5918280
Nick Main <nickmaster24@gmail.com>
2022-11-15 21:40:54 -0500

    Rick Roll

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 
 import useConfig from "./components/useConfig";
 import logo from "./logo.svg";
+import RickRoll from "./components/rickRoll";
 
 /**
  * Our Web Application
@@ -19,8 +20,10 @@ export default function App() {
       {!config.path && <p className="App-intro">You shouldn't be here yet!</p>}
       {config.path?.includes("route1") && <p>Welcome to Route 1!</p>}
       {config.path?.includes("route2") && <p>Welcome to Route 2!</p>}
+      {config.path?.includes("routeRoll") && <RickRoll />}
       <p>This lambda is now serving #{config.counter}.</p>
-      <a href={``}>Home</a> <a href={`route1`}>Route 1</a> <a href={`route2`}>Route 2</a>
+      <a href={``}>Home</a> <a href={`route1`}>Route 1</a> <a href={`route2`}>Route 2</a>{" "}
+      <a href={`routeRoll`}>Route Roll</a>
     </div>
   );
 }
---
+++ src/components/rickRoll.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export default function RickRoll() {
+  return (
+    <iframe
+      width="800"
+      height="600"
+      src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  );
+}
