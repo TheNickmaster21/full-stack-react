0ba89bd6e53df6dfe66fca6c7d1a5a0010a1a0ce
Nick Main <nickmaster24@gmail.com>
2022-11-18 20:46:09 -0500

    Minor updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,28 @@ import * as React from "react";
 import useConfig from "./components/useConfig";
 import RickRoll from "./components/RickRoll";
 
-/**
- * Our Web Application
- */
+const Links = () => (
+  <div>
+    <p className="social-link">
+      <a href="https://twitter.com/TheNickmaster21">@TheNickmaster21 on Twitter</a>
+    </p>
+
+    <p className="social-link">
+      <a href="https://www.roblox.com/users/1209469/profile/">TheNickmaster21 on Roblox</a>
+    </p>
+  </div>
+);
+
 export default function App() {
   const config = useConfig();
   return (
     <div className="App">
       <h1>NickMa.in</h1>
       {(!config.path || config.path === "/") && <p className="App-intro">You shouldn't be here yet!</p>}
-      {config.path?.includes("route1") && <p>Welcome to Route 1!</p>}
-      {config.path?.includes("route2") && <p>Welcome to Route 2!</p>}
+      {config.path?.includes("links") && <Links />}
       {config.path?.includes("routeRoll") && <RickRoll />}
-      <p>This lambda is now serving #{config.counter}.</p>
-      <a href={`/`}>Home</a> <a href={`route1`}>Route 1</a> <a href={`route2`}>Route 2</a>{" "}
-      <a href={`routeRoll`}>Route Roll</a>
+      <a href={`/`}>Home</a> <a href={`links`}>Links</a> <a href={`routeRoll`}>Route Roll</a>
+      <p className="footer">This lambda is now serving #{config.counter}.</p>
     </div>
   );
 }
